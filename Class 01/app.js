f078d4772044f8cbbf91d6532ae5fd6fc0a5a7a7
Ahsanarsh <ahsanarshad55565@gmail.com>
2024-12-12 22:52:03 +0500

    learn js

---
+++ Class 01/app.js
@@ -0,0 +1,6 @@
+// alert("Hello World");
+// alert("Hello Class ....");
+
+var a = 20;
+var b = 3030;
+alert(b);
---
+++ Class 02/app.js
@@ -0,0 +1,27 @@
+// alert("Hello Class");
+
+// variable......!!!!!
+
+// var userName = "Ahsan";
+// alert(userName);
+
+// var a = 2030;
+// alert(a);
+
+// javascript-- Data types
+
+// 1.string => A group of character = Ahsanarshad55565@
+// 2.number => 21 , 20.1
+// 3.boolean
+// 4.null
+// 5.undefined
+
+// var institute_name = "SMIT";
+// var institute_name = "SAYLANI";
+// console.log(institute_name);
+
+// var myname = "Ahsan";
+// myname = "Ahsan Ali";
+// console.log(myname);
+
+
