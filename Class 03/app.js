2900ea35bbdeb9bb398a97e217180cf7e2cf5826
Ahsanarsh <ahsanarshad55565@gmail.com>
2024-12-14 23:14:54 +0500

    learn a arthemetic operator

---
+++ Class 03/app.js
@@ -0,0 +1,128 @@
+// console.log("Hello world");
+
+// var let const
+
+// var a = "Saylani";
+// console.log("a=>", a);
+
+// var result;
+
+// result = "A+";
+// console.log("result=>", result);
+// result = "A";
+// console.log("result=>", result);
+
+// ...................................Data types..........................................
+
+// string
+// var userName = "Jaffar Aman"; // String
+// console.log(typeof userName);
+
+// number
+// var num1 = 200.9; //Number
+// console.log(typeof num1);
+
+// Boolean true | falsa
+// var isActive = true; //boolean
+// console.log(typeof isActive);
+
+// null
+// var userEmail = null;
+
+// var result = undefined;
+// var result;
+// console.log(typeof result);
+
+// ....................................Airthemetic Operator......................................
+
+// *  ==== mutiplication
+// / === Division
+// + === Addition
+// - === Substraction
+// % == modulus == Remainder
+// dmas rule
+
+// Adition
+// var num1 = 10;
+// var num2 = 21;
+// var add = num1 + num2;
+// console.log(add);
+
+// Substraction
+// var num1 = 10;
+// var num2 = 21;
+// var sub = num1 - num2;
+// console.log(sub);
+
+// Division
+// var num1 = 100;
+// var num2 = 20;
+// var division = num1 / num2;
+// console.log(division);
+
+// Multiplication
+// var num1 = 10;
+// var num2 = 21;
+// var Multiplication = num1 * num2;
+// console.log(Multiplication);
+
+// modulous
+// var num1 = 31;
+// var num2 = 3;
+// var modulous = num1 % num2;
+// console.log(modulous);
+
+// DMAS
+// var num1 = 5;
+// var num2 = 10;
+// var dmas = num1 + num2 * num2;
+// console.log(dmas);
+
+//..........BODMAS.............
+// var num1 = 5;
+// var num2 = 10;
+// var bodmas = num1 - (num1 + num2 * num1 + num1) - num1;
+// console.log(bodmas);
+
+//..........................Concatenation................................
+//.......................concatenation only work when use Addition..............................
+// concate
+// var num1 = "20";
+// var num2 = "80";
+// console.log(num1 + num2);
+
+// concate;
+// var num1 = "20";
+// var num2 = "80";
+// console.log(num1 - num2); //concatenation only work when use Addition
+
+// concate;
+// var num1 = "2a";
+// var num2 = "8";
+// console.log(num1 * num2);  // result = NaN = not a number
+
+// concate
+// var firstName = "Ahsan";
+// var lastName = "Ali";
+// console.log(firstName + lastName);
+
+// concate
+// var firstName = "Ahsan";
+// var lastName = "Ali";
+// var fullName = firstName + " " + lastName;  // empty Space bt name
+// console.log(fullName);
+
+// var num1 = 5;
+// var num2 = "10";
+// var num3 = 20;
+// console.log(num1 * num2 * num3);
+
+// var num1 = "5";
+// var num2 = 10;
+// var num3 = 20;
+// console.log(num1 + num2 + num3); // concatenation
+
+// var num1 = "5";
+// var num2 = 10;
+// var num3 = 20;
+// console.log(num1 + (num2 + num3)); // concatenation
