b8c8d1ebec1687db63bb2a0abb83670b191d91ab
Ahsanarsh <ahsanarshad55565@gmail.com>
2024-12-27 00:33:03 +0500

    learn array

---
+++ Class 06/app.js
@@ -0,0 +1,48 @@
+//....................... Gates operator ...........................
+// And operator  =>  &&
+// OR operator  => ||
+
+// var userAge = +prompt("Enter your age ");
+
+// if (userAge >= 18 && userAge <= 100) {
+//   console.log("Allow");
+// } else {
+//   console.log("Not Allow");
+// }
+
+// var htmlMarks = +prompt("Html marks");
+// var cssMarks = +prompt("CSS marks");
+// var jsMarks = +prompt("JS marks");
+// var reactMarks = +prompt("React marks");
+// var nodejsMarks = +prompt("Nodejs marks");
+
+// var obtainMarks = htmlMarks + cssMarks + jsMarks + reactMarks + nodejsMarks;
+// // console.log(obtainMarks);
+
+// var percentage = (obtainMarks / 500) * 100;
+// console.log(percentage);
+
+// if (percentage >= 90 && percentage <= 100) {
+//   console.log("A+");
+// } else if (percentage >= 80) {
+//   console.log("A");
+// } else if (percentage >= 70) {
+//   console.log("B");
+// } else if (percentage >= 60) {
+//   console.log("C");
+// } else if (percentage < 60) {
+//   console.log("Fail");
+// } else {
+//   console.log("Invalid no");
+// }
+
+// var age = 25;
+// var gender = "male";
+
+// if ((age >= 18 || age <= 40) && gender == "male") {
+//   console.log("Allow");
+// } else {
+//   console.log("Not allow");
+// }
+
+
---
+++ Class 07/app.js
@@ -0,0 +1,35 @@
+// console.log("Hello World");
+
+//...........................Arrays...........
+
+// var cityCollection = ["Karachi", "Lahore", "Multan"];
+// console.log(cityCollection);
+
+// var fruits = ["Orange", "Apple", "Avagado", "Pineaple", "Strawbery"];
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// console.log(fruits[4]);
+
+// var cities = ["lahore", "karachi"];
+// cities.push("Multan");
+// cities[2] = "Multan";
+// cities[1] = "Karachi";
+// console.log(cities);
+// console.log(cities.length);
+
+// var array = ["Karachi", "Lahore"]; // Empty Array
+// array[1] = "Multan";
+// array[2] = "Multan";
+// array[5] = "Multan";
+// array[0] = "Karachi";
+// array[1] = "Lahore";
+// console.log(array);
+// console.log(array[5]);
+
+// ..................Lenth...........
+
+// var array = ["Lahore", "Multan", "Karachi", "Hyderabad"];
+
+// console.log();
