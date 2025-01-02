30622bf63edf8328e3a4dd8b0c2af916b8efcb47
Ahsanarsh <ahsanarshad55565@gmail.com>
2025-01-02 23:16:04 +0500

    Arrays method

---
+++ Class 08/app.js
@@ -0,0 +1,41 @@
+// console.log("Hello World");
+
+// var cities = ["Karachi", "Lahore", "Multan", "Sialkot", "faislabad"];
+// console.log(cities);
+
+// console.log(cities[1]);
+
+// var arr = ["Karachi", "Lahore"];
+
+// arr[0] = "Karachi";
+// arr[1] = "Lahore";
+
+// arr[1] = "Multan";
+// console.log(arr);
+
+// var arrsys = ["Karachi", "faislabad", 1992, 804, null, undefined];
+
+// console.log(arrsys.length);
+
+//................................... Arrays Methods
+//pop .....remove last index value
+//push  ....Add value/values at last
+//shift  .....remove value first index[0] value
+//unshift .....Add value/values at start of array
+//splice  ......
+//slice
+
+// var cities = ["Karachi", "lahore"];
+// cities.pop();
+// cities.push("Multan", "Islamabad");
+// cities.unshift("Islamabad", "multan");
+// cities.shift();
+// console.log(cities);
+
+var cities = ["Karachi", "lahore", "Multan", "edinburgh"];
+// var copy = cities.slice(1, 2);
+// var copy = cities.slice(1);
+// var copy = cities.slice(0, -1);
+// var copy = cities.slice(-1);
+
+console.log(copy);
