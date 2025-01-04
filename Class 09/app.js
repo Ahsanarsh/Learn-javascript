d44fc99188838dc7c78fa588a2e9cc2b1d228587
Ahsanarsh <ahsanarshad55565@gmail.com>
2025-01-04 23:13:41 +0500

    learn splice and for loop

---
+++ Class 09/app.js
@@ -0,0 +1,18 @@
+// console.log("Hello world");
+
+// var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var copy = array.slice(2, 7);
+// var copy = array.slice(-1);
+// console.log(copy);
+
+// var cities = ["Karachi", "Lahore", "Multan", "Islamabad", "Sialkot"];
+// cities.splice(3, 0, "Faislabad");
+// cities.splice(3, 0, "Faislabad", "wwe", "aksla");
+
+// console.log(cities);
+
+//,,,,,,,,,,,,,,,,,,,,,,,,,............Loops....................
+
+for (let i = 0; i < 10; i++) {
+  console.log("Hello world");
+}
