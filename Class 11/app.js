aab2b74576dc5e955cc30fa90799e915ec4304b4
Ahsanarsh <ahsanarshad55565@gmail.com>
2025-01-09 23:04:01 +0500

    learn array with for loops

---
+++ Class 11/app.js
@@ -0,0 +1,11 @@
+// var cities = ["Karachi", "Lahore", "Multan", "Sirilanka", "Dubai", "Sialkot"];
+
+// for (let i = 0; i < cities.length; i++) {
+//   console.log(cities[i]);
+// }
+// var cities = ["Karachi", "Lahore", "Multan", "Sirilanka", "Dubai", "Sialkot"];
+
+// for (let i = cities.length - 1; i >= 0; i--) {
+//   orderNo = i + 1;
+//   document.write("<h3>" + orderNo + " " + cities[i] + "<h3/>" + "<br/>");
+// }
