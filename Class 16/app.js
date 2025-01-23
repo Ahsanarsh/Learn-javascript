23c0ef6ed4088af0d659a75b6d84293ec0b9f4fb
Ahsanarsh <ahsanarshad55565@gmail.com>
2025-01-23 23:04:40 +0500

    learn change type

---
+++ Class 16/app.js
@@ -0,0 +1,32 @@
+// let randomNumber = Math.random();
+// console.log(randomNumber);
+
+// let randomNumber = Math.floor(Math.random() * 100 + 1);
+// console.log(randomNumber);
+
+// let userNumber = +prompt("Enter Your Guess No");
+
+// let randomNumber = Math.floor(Math.random() * 10 + 1);
+
+// if (randomNumber == userNumber) {
+//   document.write("<h1>" + "You WON The Game...!" + "<h1/>");
+// } else if (randomNumber < userNumber) {
+//   document.write("<h1>" + "Your Number Is Greater...!" + "<h1/>");
+// } else if (randomNumber > userNumber) {
+//   document.write("<h1>" + "Your Number Is Less...!" + "<h1/>");
+// } else {
+//   document.write("<h1>" + "You LOSS The Game...!" + "<h1/>");
+// }
+
+// let num1 = "100";
+// let num2 = "300";
+// let add = Number(num1) + Number(num2);
+// let add = parseInt(num1) + parseInt(num2);
+// let add = +num1 + +num2;
+// console.log(add);
+
+// var num = 1231;
+// conver num into string
+// console.log(String(num));
+// console.log(typeof num.toString());
+
