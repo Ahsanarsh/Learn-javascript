83e06fb135ae27010764454953f72506a6e13503
Ahsanarsh <ahsanarshad55565@gmail.com>
2025-01-30 23:00:09 +0500

    .

---
+++ Class 18/app.js
@@ -0,0 +1,43 @@
+// new Date(); ==> data type object
+
+// let a = new Date();
+// let a = new Date().toString();
+// let a = new Date().toTimeString();
+// let a = new Date().toDateString();
+// var a = new Date().toLocaleTimeString();
+// console.log(a);
+
+// let dayOfWeek = new Date().getDay(); // it give day in number form, from 0 to 6 (0 mean sunday)(6 mean
+// saturday)
+// let getMonth = new Date().getMonth(); // it give month in number form, from 0 to 11 (0 mean january)(11 mean
+// december)
+// let getDate = new Date().getDate(); // it give date in number form, from 1 to 31
+// let getFullYear = new Date().getFullYear(); // it give full year mean 2025
+// let getHours = new Date().getHours(); // it give hours in number form, from 0 to 23
+// let getMinutes = new Date().getMinutes(); // it give minutes in number form, from 0 to 59
+// let getSeconds = new Date().getSeconds(); // it give seconds in number form, from 0 to 59
+// let getMilliseconds = new Date().getMilliseconds(); // it give milliseconds in number form, from 0 to 1000
+// console.log(getMilliseconds);
+// let now = new Date().getTime();
+
+
+//        ...............................Age calculator using date method..............
+
+
+// let userDOB = prompt("Enter your dob", "01 Jan 1990");
+
+// let userDate = new Date(userDOB).getTime();
+// // console.log(userDate);
+
+// let currentDate = new Date().getTime();
+// // console.log("Curent Date", currentDate);
+
+// let difference = currentDate - userDate;
+// // console.log(difference);
+
+// var age = difference / (1000 * 60 * 60 * 24 * 365);
+// console.log(Math.floor(age));
+
+
+//       ..................... Functions.................
+
