81d5eeff12a7ce0b16ae3aad16ad261921717ff7
Ahsanarsh <ahsanarshad55565@gmail.com>
2025-02-06 22:56:16 +0500

    learn local and global scope

---
+++ Class 21/app.js
@@ -0,0 +1,44 @@
+// function cal(num1 = 0, num2 = 0, operator = "+") {
+//   if (operator == "+") {
+//     return num1 + num2;
+//   } else if (operator == "-") {
+//     return num1 - num2;
+//   } else if (operator == "/") {
+//     return num1 + num2;
+//   } else if (operator == "*") {
+//     return num1 * num2;
+//   } else {
+//     return "Invalid Opt";
+//   }
+// }
+// let num1 = +prompt("Enter num1");
+// let num2 = +prompt("Enter num2");
+// let operator = prompt("Enter operator");
+
+// let result = cal(num1, num2, operator);
+// console.log(result);
+
+//.........................Scopes local  or global
+
+// var a = 900;
+// function foo() {
+//   var a = 10;
+//   console.log(a + " Local scope");
+// }
+// foo();
+// console.log(a + "Global Scope");
+
+// var num1 = 10;
+// function foo() {
+//   num1 = 100;
+//   console.log(num1);
+// }
+// foo();
+// console.log(num1);
+
+// function foo() {
+//   var num1 = 10;
+// }
+// foo();
+// console.log(num1);
+
