445fa16cb2d48324f83b1f9c519f585ec8a8b19c
Ahsanarsh <ahsanarshad55565@gmail.com>
2025-02-08 18:09:43 +0500

    learn getelementbyid

---
+++ Class 22/app.js
@@ -0,0 +1,27 @@
+// function greet() {
+//   alert("Hello World");
+// }
+
+// function getValue() {
+// console.log("getValue");
+
+//   let userName = document.getElementById("userName");
+//   console.log("userName" + userName.value);
+
+//   let userName = document.getElementById("userName");
+//   let password = document.getElementById("password");
+
+//   console.log(userName.value);
+//   console.log(password.value);
+// }
+
+function setValue() {
+  //   console.log("setValue");
+
+  let userName = document.getElementById("userName");
+  let password = document.getElementById("password");
+
+  userName.value = "JaffarAman";
+  password.value = "123456";
+  
+}
