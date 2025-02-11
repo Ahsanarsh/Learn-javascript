c5218099161d4884606e91e6d317bd32935a1498
Ahsanarsh <ahsanarshad55565@gmail.com>
2025-02-11 22:59:02 +0500

    setValue and getValue

---
+++ Class 23/app.js
@@ -0,0 +1,13 @@
+function getValue() {
+  let userName = document.getElementById("userName-1");
+  let password = document.getElementById("password-1");
+  console.log(userName.value);
+  console.log(password.value);
+}
+
+function setValue() {
+  let userName = document.getElementById("userName");
+  let password = document.getElementById("password");
+  userName.value = "Ahsan";
+  password.value = "Askdnlsn";
+}
