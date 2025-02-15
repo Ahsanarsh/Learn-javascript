4352f0fc255960aeef66968d1d346434a4a1c6aa
Ahsanarsh <ahsanarshad55565@gmail.com>
2025-02-15 22:58:57 +0500

    learn onclick event

---
+++ Class 24/app.js
@@ -0,0 +1,37 @@
+// function getValue() {
+//   let email = document.getElementById("email");
+//   let password = document.getElementById("password");
+//   console.log(email.value);
+//   console.log(password.value);
+
+// }
+
+// function setValue() {
+//   let email = document.getElementById("email");
+//   let password = document.getElementById("password");
+//   email.value = "ahsanarshad55565@gmail.com";
+//   password.value = "122456";
+// }
+
+// function showPassword() {
+//   let password = document.getElementById("password");
+//   // password.type = "text";
+//   if (password.type == "password") {
+//     password.type = "text";
+//   } else {
+//     password.type = "password";
+//   }
+// }
+
+// function getParra() {
+//   let parra = document.getElementById("parra");
+//   console.log(parra.innerHTML);
+// }
+// function setParra() {
+//   let parra = document.getElementById("parra");
+//   parra.innerHTML = "para value";
+// }
+
+function foo() {
+  console.log("fooo ");
+}
