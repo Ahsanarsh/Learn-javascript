b05b850e4248bdc1a3345481d8d1a6f7212d637a
Ahsanarsh <ahsanarshad55565@gmail.com>
2025-03-04 08:12:16 +0500

    learn focus using js

---
+++ Class 29/app.js
@@ -0,0 +1,11 @@
+// function changeImg(kuchb) {
+//   console.log(kuchb);
+//   kuchb.src = "./pexels-photo-5593539.jpeg";
+//   var img = document.getElementById("img");
+//   img.src = "./pexels-photo-5593539.jpeg";
+// }
+
+function bulbhandler() {
+  var bulbimg = document.getElementById("bulbimg");
+  bulbimg.src="Class 29\pexels-photo-577514.webp"
+}
---
+++ Class 30/app.js
@@ -0,0 +1,25 @@
+// let onFocus = document.querySelector("#onFocus");
+
+// onFocus.addEventListener("focus", function () {
+//   onFocus.style.backgroundColor = "darkgrey";
+// });
+// onFocus.addEventListener("blur", function () {
+//   onFocus.style.backgroundColor = "white";
+// });
+
+// let inputfield = document.querySelector("#onblur");
+
+// inputfield.addEventListener("blur", function (e) {
+//   if (inputfield.value.length >= 3) {
+//     inputfield.style.backgroundColor = "2px solid grey";
+//   }
+// });
+
+// let bigText = document.querySelector("#bigText");
+
+// bigText.addEventListener("click", function () {
+//   let para = document.querySelector(".para");
+//   console.log(para);
+//   para.style.fontSize = "22px";
+//   para.className += " bigText";
+// });
