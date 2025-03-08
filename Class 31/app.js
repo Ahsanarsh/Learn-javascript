135c8493a32a600617af4c5ba3c44e027c7b6eea
Ahsanarsh <ahsanarshad55565@gmail.com>
2025-03-08 08:14:53 +0500

    learn tagName and ClassName

---
+++ Class 31/app.js
@@ -0,0 +1,13 @@
+// let parras = document.getElementsByTagName("p");
+
+// // console.log(parras);
+// for (let i = 0; i < parras.length; i++) {
+//   console.log(parras[i].innerHTML);
+//   parras[i].style.backgroundColor = "red";
+// }
+
+// let parent = document.getElementById("parent");
+// console.log(parent);
+// console.log(parent.getElementByTagName("p"));
+// let parent1 = parent.getElementByTagName("p");
+// console.log(parent1);
