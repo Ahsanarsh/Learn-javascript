0e11845391e3d4cc9ce806e31e580755fc4537ae
Ahsanarsh <ahsanarshad55565@gmail.com>
2025-03-13 08:02:34 +0500

    learn dom

---
+++ Class 32/app.js
@@ -0,0 +1,4 @@
+// console.log(document);
+// console.log(document.childNodes);
+// console.log(document.childNodes[1]);
+console.log(document.childNodes[1].childNodes);
---
+++ Class 33/app.js
@@ -0,0 +1,11 @@
+let parent = document.getElementById("parent");
+
+// console.log(parent.children);
+
+//.................Way to get child of parent.............
+
+// console.log(parent.firstChild);// ............. Wrong Aproach
+// console.log(parent.firstElementChild); //.............. Right Aproach
+// console.log(parent.lastElementChild);// .............to get last child
+// console.log("Parent", parent.firstElementChild.nextElementSibling);
+// console.log(parent.firstElementChild.parentNode);
