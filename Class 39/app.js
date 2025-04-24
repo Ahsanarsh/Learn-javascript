c3a708c473748b1807d24ab8b94c1f43ecd4f587
Ahsanarsh <ahsanarshad55565@gmail.com>
2025-04-24 22:47:55 +0500

    learn constructor

---
+++ Class 39/app.js
@@ -0,0 +1,37 @@
+// let std1 = {
+//   name: "Ahsan",
+//   id: 657876,
+//   age: 23,
+//   email: "ahsanarshad55565@mail.com",
+// };
+
+// let userName = document.getElementById("userName");
+
+// userName.innerHTML = "Hello " + std1.name;
+
+// function Std(firstName, lastName, age, course, email) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.stdage = age;
+//   this.stdcourse = course;
+//   this.stdemail = email;
+//   this.fullName = function () {
+//     return this.firstName + " " + this.lastName;
+//   };
+// }
+
+// let std1 = new Std("Ahsan", "Arshad");
+// let std2 = new Std("Ali", "Albha");
+
+// console.log(std1);
+// console.log(std2);
+
+// function Course(name, days, campus, time) {
+//   this.stdName = name;
+//   this.days = days;
+//   this.campus = campus;
+//   this.time = time;
+// }
+// let course1 = new Course("Ahsan", ["mon", "wed", "fri"], "ZA", "9 to 11");
+// console.log(course1);
+
